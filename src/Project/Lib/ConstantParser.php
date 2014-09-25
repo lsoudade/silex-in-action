aa562cfd0b86d8e4ab84b5614e238145b0e190b5
Lorraine Soudade <lsoudade@mediastay.com>
2014-09-25 15:39:39 +0200

    fixed bug

--- src/Project/Lib/ConstantParser.php
+++ src/Project/Lib/ConstantParser.php
@@ -12,7 +12,7 @@ class ConstantParser
         $refl = new \ReflectionClass($class);   
         
         return array_flip(array_filter(array_flip($refl->getConstants()), function($i) use($pattern) {
-            return strstr($i, $pattern);
+            return stripos($i, $pattern) === 0;
         }));
     }
 }
